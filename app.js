3494d9dd8ca6eb63fc9bc719d73b8ae0b8ccf4f5
vjetphonggm <vjetphonggm@gmail.com>
2024-09-27 00:12:41 +0700

    Punnez

--- app.js
+++ app.js
@@ -424,3 +424,14 @@ document.addEventListener("DOMContentLoaded", function () {
         controls.appendChild(row2);
     }
 });
+
+// Check if service workers are supported, then register the service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+            console.log('Service Worker registered with scope:', registration.scope);
+        }, function (err) {
+            console.log('Service Worker registration failed:', err);
+        });
+    });
+}  
