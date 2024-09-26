4fa9c6ac1dddc28bc164c1b99e759f559b5d9c36
vjetphonggm <vjetphonggm@gmail.com>
2024-09-27 06:46:12 +0700

    Punnez

--- app.js
+++ app.js
@@ -428,10 +428,10 @@ document.addEventListener("DOMContentLoaded", function () {
 // Check if service workers are supported, then register the service worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+        navigator.serviceWorker.register('./service-worker.js').then(function (registration) {
             console.log('Service Worker registered with scope:', registration.scope);
         }, function (err) {
             console.log('Service Worker registration failed:', err);
         });
     });
-}  
+}
--- service-worker.js
+++ service-worker.js
@@ -3,14 +3,14 @@ self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open('flashcard-cache-v1').then(function (cache) {
             return cache.addAll([
-                '/',                       // Root URL (index.html)
-                '/index.html',             // Main HTML file
-                '/css/base.css',           // CSS file
-                '/css/style.css',          // Another CSS file
-                '/img/logo.png',           // Logo image
-                '/img/icon-192x192.png',   // App icon (small)
-                '/img/icon-512x512.png',   // App icon (large)
-                '/app.js'                  // Main JavaScript file
+                './',                       // Root URL (index.html)
+                './index.html',             // Main HTML file
+                './css/base.css',           // CSS file
+                './css/style.css',          // Another CSS file
+                './img/logo.png',           // Logo image
+                './img/icon-192x192.png',   // App icon (small)
+                './img/icon-512x512.png',   // App icon (large)
+                './app.js'                  // Main JavaScript file
             ]);
         })
     );
